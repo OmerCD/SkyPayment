baf6ce9a07aae8ff79f8663711a2e1fe6a1fccc8
turgut.karadag <turgut_kara_dag@hotmail.com>
2020-09-28 23:08:37 +0300

    Servis ve basit login eklendi.

---
+++ SkyPayment.Management.API/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SkyPayment.Contract.RequestModel;
+
+namespace SkyPayment.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [Route("auth")]
+        public IActionResult Login (LoginModel model)
+        {
+            /* Sadece test. Lütfen dalga geçmeyin.*/
+            if (model.UserName=="user")
+            {
+                if (model.Password=="123")
+                {
+                    return Ok();
+                }
+            }
+
+            return BadRequest();
+        }
+    }
+}
