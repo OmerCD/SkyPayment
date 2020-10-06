b178ec0d1baf1fba3912bbabae6d4b70f1b939de
OmerC <omer.danacioglu@zanhagrup.com>
2020-10-06 17:41:59 +0300

    Changes

---
+++ SkyPayment.Mappings/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AutoMapper;
+
+namespace SkyPayment.Mappings
+{
+    public class AuthenticationMapping : Profile
+    {
+        public AuthenticationMapping()
+        {
+            
+        }
+    }
+}
